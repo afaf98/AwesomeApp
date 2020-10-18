6bd481acb61d808bd023bafdbc3b02ce26a11bb5
Afaf Ibrahimi <afaf.ibrahimi@gmail.com>
2020-10-18 15:37:48 +0200

    Components and Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Title from "./components/Title";
 
 function App() {
   return (
     <div className="App">
-      <p>This looks Fun!</p>
+      <main>
+        <Title content="Some Simple Title..." />
+      </main>
       <img src={logo} className="App-logo" alt="logo" />
       <p>
         Edit <code>src/App.js</code> and save to reload.
